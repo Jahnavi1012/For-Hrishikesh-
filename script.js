125d597a8d1e9ea3f2f28b5992e1ee23661c6ff2
Jahnavi1012 <jahnavikaushal1012@gmail.com>
2026-02-13 16:01:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ document.getElementById("lockScreen").style.display = "none";
 
 document.getElementById("mainContent").style.display = "block";
 
+// ADD THESE 2 LINES ↓↓↓
+let music = document.getElementById("bg-music");
+music.play();
+
 }
 else{
 document.getElementById("error").innerHTML = "Wrong password";
@@ -18,7 +22,5 @@ document.getElementById("error").innerHTML = "Wrong password";
 }
 
 function reveal(){
-
 document.getElementById("hiddenMessage").style.display = "block";
-
 }
