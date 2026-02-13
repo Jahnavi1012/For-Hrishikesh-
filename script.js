bbd8181992c1026f9c3f70663c3c5be48aa24c5f
Jahnavi1012 <jahnavikaushal1012@gmail.com>
2026-02-13 15:16:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,29 +10,9 @@ document.getElementById("lockScreen").style.display = "none";
 
 document.getElementById("mainContent").style.display = "block";
 
-typeWriter();
-
 }
 else{
 document.getElementById("error").innerHTML = "Wrong password";
-}
-
-}
-
-const text = "I miss you every day, and distance has never changed what you mean to me.";
-
-let i = 0;
-
-function typeWriter(){
-
-if(i < text.length){
-
-document.getElementById("typeText").innerHTML += text.charAt(i);
-
-i++;
-
-setTimeout(typeWriter, 50);
-
 }
 
 }
